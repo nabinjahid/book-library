df1d7f213b4d940241b91ccf1cd15e02ed7c5506
Md Jahidul Islam <nabinjahid@gmail.com>
2024-11-03 18:01:20 +0600

    project setup done with react router dom, tailwind css and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+     
+      
+<input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </>
+  )
+}
+
+export default App
